cbe4b26ea8e5590e9490b0de1feb48152a43e6fa
IT Educator <ahmedbelaiba19952018@gmail.com>
2021-01-26 14:41:45 +0100

    authentification-angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './web/auth/auth.component';
+import { LoginComponent } from './web/auth/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'auth',
+    component: AuthComponent,
+    canActivate: [UnAuthGuardService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './web/auth/auth.component';
+import { LoginComponent } from './web/auth/login/login.component';
+import { DashboardComponent } from './web/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,

4d1e93689bbe18e121323c2f48f924d23d011e5c
Amine BEN REJEB <amine.benrejeb@redlean.io>
2021-02-05 01:23:07 +0100

    added url talendEsb

--- django/API/urls.py
+++ django/API/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('accounts.urls')),
-    path('api/', include('core.urls'))
+    path('api/', include('core.urls')),
+    path('talendEsb', include('talendEsb.urls'))
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

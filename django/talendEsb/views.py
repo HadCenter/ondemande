1a5b792f2bbb39a9a0bf1ea31048dbf03bf6a00f
Amine BEN REJEB <amine.benrejeb@redlean.io>
2021-02-05 10:43:22 +0100

    view talendESB

--- django/talendEsb/views.py
+++ django/talendEsb/views.py
@@ -3,15 +3,16 @@ from django.views.decorators.csrf import csrf_exempt
 import requests
 import asyncio
 
-talendUrl = 'https://www.w3schools.com/python/demopage.php'
-testObj = {
-	"filePath": "path",
-	"ClientOwner" : "amine",
-	"fileId" : "ehklaaze"
-}
+talendUrl = 'https://webhooks.eu.cloud.talend.com/onDemandESB/e6cb39ecec634b44b99b40ab36eda213'
+testObj = [{
+	"filePath": "25_12_2020_EDI_2021-02-05-05-36-00.xlsx",
+	"ClientOwner" : 3,
+	"fileId" : 1
+}]
+
 
 @csrf_exempt
 def startEngineOnEdiFiles(request):
-
-    return HttpResponse("Hello, world. starting engine.")
+	requests.post(talendUrl,data = testObj)
+	return HttpResponse("Hello, world. starting engine.")
 

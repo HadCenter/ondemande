46ec3c78b630e4a2272342a15a84d0dc79f6de89
IT Educator <ahmedbelaiba19952018@gmail.com>
2021-05-28 07:39:14 +0100

    initialize page magistor

--- angular/angular-app/src/app/pages/magistor/magistor.module.ts
+++ angular/angular-app/src/app/pages/magistor/magistor.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { MagistorRoutingModule } from './magistor-routing.module';
 import { MagistorComponent } from './magistor.component';
-
+import { ThemeModule } from 'theme';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 
 @NgModule({
   declarations: [MagistorComponent,],
   imports: [
     CommonModule,
-    MagistorRoutingModule
+    MagistorRoutingModule,
+    ThemeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
   ]
 })
 export class MagistorModule { }
--- angular/angular-app/src/app/pages/magistor/magistor.service.ts
+++ angular/angular-app/src/app/pages/magistor/magistor.service.ts
@@ -5,5 +5,62 @@ import { Injectable } from '@angular/core';
 })
 export class MagistorService {
 
-  constructor() { }
+    constructor() { }
+  public getAdvancedHeaders() {
+    return [
+      {
+        name: 'Fichier',
+        sort: 0,
+
+      },
+      {
+        name: 'Date création',
+        sort: 0,
+
+      },
+      {
+        //             name: '# Code client',
+        name: 'Nom client',
+        sort: 0,
+
+      },
+      {
+        name: 'Prestations erronées',
+        sort: null,
+
+      },
+      {
+        name: 'Prestations validées',
+        sort: null,
+
+      },
+      {
+        name: 'Statut',
+        sort: null,
+
+      },
+      {
+        name: 'Actions',
+        sort: null,
+
+      }
+
+    ];
+  }
+  public advanceTableData: any = [{
+    fichier: "2", date_creation: "28/05/2021", nom_client: "rutabago",prest_erron: 5, prest_valide: 4, statut: "Terminé"
+  },
+  {
+    fichier: "1", date_creation: "27/05/2021", nom_client: "infarm",prest_erron: 10, prest_valide: 4, statut: "Terminé"
+  }
+  ]
+
+  public getAdvancedTableNumOfPage(countPerPage)
+  {
+    return Math.ceil(this.advanceTableData.length / countPerPage);
+  }
+  public getAdvancedTablePage(page, countPerPage)
+  {
+    return this.advanceTableData.slice((page - 1) * countPerPage, page * countPerPage);
+  }
 }

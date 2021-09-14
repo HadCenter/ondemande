0d402d0980bc9b542b5ca478edc08ad688332f34
aya_kilani <aya.kilani@redlean.io>
2021-09-14 11:23:37 +0200

    Changer format date

--- angular/angular-app/src/app/pages/home/home.component.ts
+++ angular/angular-app/src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { Observable } from "rxjs";
 import { map, startWith } from "rxjs/operators";
 
+
 export class Client {
   constructor(public clientName: string, public selected?: boolean) {
     if (selected === undefined) selected = false;

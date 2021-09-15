d65a0c68f753af2e4ad27be613a77c04eb85c4c0
aya_kilani <aya.kilani@redlean.io>
2021-09-15 12:22:54 +0200

    Fix bug anomalie +

--- angular/angular-app/src/app/pages/home/home.component.ts
+++ angular/angular-app/src/app/pages/home/home.component.ts
@@ -190,6 +190,7 @@ export class HomeComponent extends UpgradableComponent implements OnInit {
         }
         this.selectedFilesNames = this.selectedFiles.map(file => file.fileName);
         this.fileControl.setValue('');
+        this.changeTypeAomaliesList();
         // console.warn("Fin *********** toggleSelectionFile")
       }else{
         if (selected instanceof Anomalie)
@@ -211,6 +212,21 @@ export class HomeComponent extends UpgradableComponent implements OnInit {
         }
       }
     }
+  }
+  changeTypeAomaliesList(){
+   // const listAnomalies=[];
+    this.listTypesAnomaliesNames=[];
+    this.selectedFilesNames.forEach(el => {
+     const listAnomalies=this.allAnomalies_copy.filter(element => element.edi_file_name ==el);
+    listAnomalies.forEach(element => {
+      var anomalie = new Anomalie(element.anomalie_name);
+      this.listTypesAnomaliesNames.push(anomalie);
+     this.listTypesAnomaliesNames = this.listTypesAnomaliesNames.filter((v, i, a) => a.findIndex(t => (t.anomalieName === v.anomalieName)) === i);
+    });
+  });
+
+  this.initializeFiltredTypesAnomalies();
+
   }
   getAllClients() {
     const uniqueClients = this.allAnomalies_copy.filter((v, i, a) => a.findIndex(t => (t.client_id === v.client_id)) === i);

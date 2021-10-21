d50ba8196be6ae9912e3ef45897764fbaaa2ca51
aya_kilani <aya.kilani@redlean.io>
2021-10-21 20:57:35 +0200

    Change format date pop up

--- angular/angular-app/src/app/pages/list-transaction/list-transaction.module.ts
+++ angular/angular-app/src/app/pages/list-transaction/list-transaction.module.ts
@@ -6,15 +6,16 @@ import { ThemeModule } from 'theme';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { ListTransactionService } from './list-transaction.service';
 import { GenererTransactionService } from './dialog/generer-transaction.service';
 import { LoaderModule } from 'app/components/loader/loader.module';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { Mypipe } from './mypipe';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 @NgModule({
-  declarations: [ListTransactionComponent,DailogGenerateTransaction,Mypipe],
+  declarations: [ListTransactionComponent, DailogGenerateTransaction, Mypipe],
   imports: [
     CommonModule,
     ListTransactionRoutingModule,
@@ -36,6 +37,7 @@ import { Mypipe } from './mypipe';
     ListTransactionService,
     GenererTransactionService,
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' },
   ],
 })
 export class ListTransactionModule { }

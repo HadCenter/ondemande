5b91f02142082a41bbc8c588562cadaae79f6cdc
med jawhar nabi <mohamedjawhar.nabi@redlean.io>
2022-01-06 17:05:56 +0100

    fix powerbi urls /

--- django/embededPowerBI/urls.py
+++ django/embededPowerBI/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("getSingleReport/<str:id>", views.getEmbedParamsForSingleReport, name = "get-single-report" ),
-    path("getAllReports/", views.getMultipleReports, name = "get-single-report" ),
-    path("getUserToken/", views.getUserToken, name = "get-user-token" ),
-    path("resume/", views.resume, name = "resume-capacity" ),
-    path("suspend/", views.suspend, name = "suspend-capacity" ),
-    path("getCapacityState/", views.getCapacityState, name = "get-capacity-state" ),
+    path("getAllReports", views.getMultipleReports, name = "get-single-report" ),
+    path("getUserToken", views.getUserToken, name = "get-user-token" ),
+    path("resume", views.resume, name = "resume-capacity" ),
+    path("suspend", views.suspend, name = "suspend-capacity" ),
+    path("getCapacityState", views.getCapacityState, name = "get-capacity-state" ),
     path("refreshReport/<str:id>", views.refreshReport, name = "refresh-report" ),
     path("getRefreshState/<str:id>", views.getRefreshState, name = "refresh-report" ),
     path("refreshDatabase", views.refreshDatabase, name = "refresh-database" ),

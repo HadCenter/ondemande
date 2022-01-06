666183918f63b5863d111f0a73a50b4bf3e43198
med jawhar nabi <mohamedjawhar.nabi@redlean.io>
2022-01-06 11:38:42 +0100

    change wss to ws

--- angular/angular-app/src/environments/environment.prod.ts
+++ angular/angular-app/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiBaseUrl: 'https://ondemand.ecolotrans.net/back',
-  WS_URL: 'wss://ondemand.ecolotrans.net/back/ws/notifications'
+  WS_URL: 'ws://ondemand.ecolotrans.net/back/ws/notifications'
 };

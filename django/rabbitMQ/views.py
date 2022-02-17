a664b811afc3b8b8cd3e60cf5b43094290e7d368
med jawhar nabi <mohamedjawhar.nabi@redlean.io>
2022-02-17 10:40:23 +0100

    comment rabbitmq views

--- django/rabbitMQ/views.py
+++ django/rabbitMQ/views.py
@@ -31,34 +31,34 @@ from rabbitMQ.EnvironnementJobEnded import start_JobEnded_consumer
 credentials = pika.PlainCredentials('admin', 'password')
 #connection = pika.BlockingConnection(pika.ConnectionParameters(host='52.47.208.8',port= 5672 ,credentials = credentials) )
 #connection = pika.BlockingConnection(pika.URLParameters('amqp://admin:password@52.47.208.8:5672/%2F') )
-connection = pika.BlockingConnection(pika.URLParameters('amqps://ucuvenkg:g8SXY-tERqixsWjG1PL6N-BP98jsU5fH@rat.rmq2.cloudamqp.com/ucuvenkg'))
+# connection = pika.BlockingConnection(pika.URLParameters('amqps://ucuvenkg:g8SXY-tERqixsWjG1PL6N-BP98jsU5fH@rat.rmq2.cloudamqp.com/ucuvenkg'))
 
-channel = connection.channel()
+# channel = connection.channel()
 
-channel.queue_declare(queue='converge.queue.job.start',durable=True)
-consumer_thread = threading.Thread(target=start_consumer)
-consumer_thread2 = threading.Thread(target=start_JobEnded_consumer)
+#channel.queue_declare(queue='converge.queue.job.start',durable=True)
+# consumer_thread = threading.Thread(target=start_consumer)
+# consumer_thread2 = threading.Thread(target=start_JobEnded_consumer)
 #thead 1 ordonnanceur
-consumer_thread.start()
+# consumer_thread.start()
 #thread 2 actualisation automatique
 #desactiver thread 1 et 2 sur localhost
-consumer_thread2.start()
+# consumer_thread2.start()
 
 
-def sendMessageRabbitMqToStartJob(message : str):
-    global connection
-    global channel
-    try:
-        channel.basic_publish(exchange='job.to.start',
-                              routing_key='',
-                              body=message)
-    except :
+# def sendMessageRabbitMqToStartJob(message : str):
+#     global connection
+#     global channel
+#     try:
+#         channel.basic_publish(exchange='job.to.start',
+#                               routing_key='',
+#                               body=message)
+#     except :
 
-        if connection.is_closed :
-            connection = pika.BlockingConnection(pika.URLParameters('amqps://ucuvenkg:g8SXY-tERqixsWjG1PL6N-BP98jsU5fH@rat.rmq2.cloudamqp.com/ucuvenkg'))
-        if channel.is_closed:
-            channel = connection.channel()
-        channel.basic_publish(exchange='job.to.start',
-                          routing_key='',
-                          body=message)
+#         if connection.is_closed :
+#             connection = pika.BlockingConnection(pika.URLParameters('amqps://ucuvenkg:g8SXY-tERqixsWjG1PL6N-BP98jsU5fH@rat.rmq2.cloudamqp.com/ucuvenkg'))
+#         if channel.is_closed:
+#             channel = connection.channel()
+#         channel.basic_publish(exchange='job.to.start',
+#                           routing_key='',
+#                           body=message)
 

7a42f267dca2d6e2c367ff626b540f1e5c28638f
aya_kilani <aya.kilani@redlean.io>
2022-02-24 09:28:24 +0100

    Fix search on 'prestations validées'

--- angular/angular-app/src/app/pages/magistor/details-file-magistor/details-file-magistor.component.ts
+++ angular/angular-app/src/app/pages/magistor/details-file-magistor/details-file-magistor.component.ts
@@ -438,7 +438,7 @@ export class DetailsFileMagistorComponent implements OnInit {
   }
 
   filterItemsValid(filterValue: string) {
-    return this.copyfileMagistor.filter((item) => {
+    return this.files.filter((item) => {
       return JSON.stringify(item).toLowerCase().includes(filterValue.toLowerCase());
     });
   }

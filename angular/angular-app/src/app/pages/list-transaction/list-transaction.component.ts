740d65ecf99dc9cc13007393bc362ae7e02d0908
aya_kilani <aya.kilani@redlean.io>
2022-03-02 11:42:33 +0100

    Blocage génération (traitement du cas en cours)

--- angular/angular-app/src/app/pages/list-transaction/list-transaction.component.ts
+++ angular/angular-app/src/app/pages/list-transaction/list-transaction.component.ts
@@ -47,7 +47,7 @@ export class ListTransactionComponent implements OnInit {
       const end = ((element.end_date.substr(0, 19)).split('-'));
       var thisDate = creatAt[2].split('T');
       var time = thisDate[1].split(':');
-      var currentTimeZoneOffsetInHours = ((new Date().getTimezoneOffset() / 60));      
+      var currentTimeZoneOffsetInHours = ((new Date().getTimezoneOffset() / 60));
       time[0] = (parseInt(time[0]) - currentTimeZoneOffsetInHours).toString();  //affich l'heure selon l'heure de pc de l'user
       thisDate[1] = [time[0], time[1], time[2]].join(":");
       var thisDate2 = start[2].split('T');
@@ -291,14 +291,14 @@ export class DailogGenerateTransaction {
     const formData = new FormData();
     formData.append('start_date', start_date);
     formData.append('end_date',end_date);
-    console.warn("start_date =",start_date);
-    console.warn("end_date =",end_date);
+    // console.warn("start_date =",start_date);
+    // console.warn("end_date =",end_date);
     const start = moment(start_date, 'YYYY-MM-DD');
     const end   = moment(end_date, 'YYYY-MM-DD');
     const rangeTransaction = moment.range(start, end);
     var rangeExist = false;
     this.receivedTransactionsFromParentComponent.copy_transactions.forEach(element => {
-      if (element.statut == "En attente")
+      if (element.statut == "En attente" || element.statut == "En cours")
       {
          var formatStartDate = element.start_date.split("-").reverse().join("-");
          var formatEndDate = element.end_date.split("-").reverse().join("-");
@@ -307,6 +307,7 @@ export class DailogGenerateTransaction {
            rangeExist = true;
          }
       }
+      
     });
     if (rangeExist)
     {

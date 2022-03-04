9c0ac7ace4a0e223b17ea0c4b78b1a1aa33bf0dc
med jawhar nabi <mohamedjawhar.nabi@redlean.io>
2022-03-04 13:37:32 +0100

    LOT1-719 Ajout de téléchargemnt  fichier livraison

--- django/talendEsb/urls.py
+++ django/talendEsb/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('getSingleTransactionMadLivraison/<int:pk>', views.getSingleTransactionMadLivraison),
     path('startEngineOnMagistorFiles', views.startEngineOnMagistorFiles),
     path('updateMetaDataFileInTableTransactionsLivraison', views.updateMetaDataFileInTableTransactionsLivraisonWS, name="updateMetaDataFileInTableTransactionsLivraison"),
+    path('downloadLivraisonFile', views.downloadLivraisonFile, name= "downloadLivraisonFile"),
 
 ]
